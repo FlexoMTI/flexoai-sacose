d5385d587361d7aeeb07642c706720fac62d617e
FlexoMTI <katakata94@gmail.com>
2025-06-17 09:54:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Sau pune doar domeniul tău WordPress dacă vrei restricție
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from utils.excel_loader import load_data
